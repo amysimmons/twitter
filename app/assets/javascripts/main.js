76e67703feb25c86e90115b5dabe0dc351b493bd
Amy Simmons <amy__simmons@hotmailcom>
2015-02-09 18:32:57 +1100

    new tweet countdown working

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,21 @@
+
+function updateCountdown() {
+    // 140 is the max message length
+
+    var remaining = 140 - $('.new-tweet-content').val().length;
+
+    $('.new-tweet-counter').text(remaining + ' characters remaining');
+
+    if (remaining == 0 ) {
+        $( ".new-tweet-content" ).effect( "shake" );
+    }
+
+}
+
+$(document).ready(function($) {
+    updateCountdown();
+    $('.new-tweet-content').change(updateCountdown);
+    $('.new-tweet-content').keyup(updateCountdown);
+});
+
+
