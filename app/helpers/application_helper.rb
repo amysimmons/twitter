8bae33e1bf96c9a06f06836097b164bcd0f05a95
Amy Simmons <amy__simmons@hotmailcom>
2015-02-12 12:07:15 +1100

    homepage login signup  styled

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,10 @@
 module ApplicationHelper
     def nav_menu
-        # links = "<li>#{link_to('Home', root_path)}</li>"
-
-        logo = "<i class='fa fa-twitter'></i>"
-
-  
-
+        logo = "<a href='#{root_path}'><i class='fa fa-twitter'></i></a>"
         if @current_user.present?
-
             # if@current_user.is_admin?
             #     links += "<li>#{link_to('All users', users_path)}</li>"
             # end
-
             links = "<li>#{link_to('Home', root_path)}</li>"
             links += "<li>#{link_to('@' + @current_user.username, username_path(@current_user.username))}</li>"
             links += "<li>#{ link_to('New tweet ', new_tweet_path)}</li>"
@@ -19,8 +12,6 @@ module ApplicationHelper
             logo + links
         else
             logo
-            # links += "<li>#{link_to('Sign up', new_user_path)}</li><li>#{link_to('Log in', login_path)}</li>"
         end
-
     end
 end
